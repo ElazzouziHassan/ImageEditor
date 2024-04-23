cf82317330ab4859a410b8ddf1ecac8f7b580b97
wizardy <wizardhassan99@gmail.com>
2024-04-23 11:16:49 +0100

    commit

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,16 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
- 
-export default clerkMiddleware();
- 
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher([
+  '/',
+  '/profile(.*)',
+  '/coins(.*)',
+  '/editing/(.*)',
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
+
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
