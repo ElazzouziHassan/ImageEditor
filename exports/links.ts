7190765626f41cadf0bc5e04c920a697af183a8b
wizardy <wizardhassan99@gmail.com>
2024-04-24 10:45:54 +0100

    commit

---
+++ exports/links.ts
@@ -0,0 +1,42 @@
+export const Links = [
+  {
+    label: "Home",
+    route: "/",
+    icon: "/assets/icons/icon.svg",
+  },
+  {
+    label: "Image Restore",
+    route: "/transformations/add/restore",
+    icon: "/assets/icons/icon.svg",
+  },
+  {
+    label: "Generative Fill",
+    route: "/transformations/add/fill",
+    icon: "/assets/icons/icon.svg",
+  },
+  {
+    label: "Object Remove",
+    route: "/transformations/add/remove",
+    icon: "/assets/icons/icon.svg",
+  },
+  {
+    label: "Object Recolor",
+    route: "/transformations/add/recolor",
+    icon: "/assets/icons/icon.svg",
+  },
+  {
+    label: "Background Remove",
+    route: "/transformations/add/removeBackground",
+    icon: "/assets/icons/icon.svg",
+  },
+  {
+    label: "Profile",
+    route: "/profile",
+    icon: "/assets/icons/icon.svg",
+  },
+  {
+    label: "Buy Credits",
+    route: "/credits",
+    icon: "/assets/icons/icon.svg",
+  },
+];
