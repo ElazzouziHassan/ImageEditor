141fe3ef5e780fee3b6fb83a216c1cbd0575c88e
wizardy <wizardhassan99@gmail.com>
2024-04-24 11:16:19 +0100

    commit

--- components/shared/SideBar.tsx
+++ components/shared/SideBar.tsx
@@ -1,17 +1,77 @@
+"use client"
+import { navLinks } from '@/constants'
+import { SignedIn, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import { usePathname } from 'next/navigation'
 
-function SideBar() {
+const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
-    <aside className='side-bar'>
+    <aside className="side-bar">
       <div className="flex size-full flex-col gap-4">
-        <Link href='/' className="side-bar-logo">
-          <Image src="/assets/images/logo.png" alt='Smart Canvas Logo' width={180} height={28} />
+        <Link href="/" className="side-bar-logo">
+          <Image src="/assets/images/logo.svg" alt="Smart Canvas Logo" width={180} height={28} />
         </Link>
+
+        <nav className="side-bar-nav">
+          <SignedIn>
+            <ul className="side-bar-nav_elements">
+              {navLinks.slice(0, 6).map((link) => {
+                const isActive = link.route === pathname
+
+                return (
+                  <li key={link.route} className={`side-bar-nav_element group ${
+                    isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'
+                  }`}>
+                    <Link className="side-bar-link" href={link.route}>
+                      <Image 
+                        src={link.icon}
+                        alt="logo"
+                        width={24}
+                        height={24}
+                        className={`${isActive && 'brightness-200'}`}
+                      />
+                      {link.label}
+                    </Link>
+                  </li>
+                )
+              })}
+              </ul>
+
+
+            <ul className="side-bar-nav_elements">
+              {navLinks.slice(6).map((link) => {
+                const isActive = link.route === pathname
+
+                return (
+                  <li key={link.route} className={`side-bar-nav_element group ${
+                    isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'
+                  }`}>
+                    <Link className="side-bar-link" href={link.route}>
+                      <Image 
+                        src={link.icon}
+                        alt="logo"
+                        width={24}
+                        height={24}
+                        className={`${isActive && 'brightness-200'}`}
+                      />
+                      {link.label}
+                    </Link>
+                  </li>
+                )
+              })}
+
+              <li className="flex-center cursor-pointer gap-2 p-4">
+                <UserButton afterSignOutUrl='/' showName />
+              </li>
+            </ul>
+          </SignedIn>
+        </nav>
       </div>
     </aside>
   )
 }
 
-export default SideBar
+export default Sidebar
