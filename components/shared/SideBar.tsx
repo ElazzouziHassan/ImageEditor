5739fe313463b8912ae4098f1fd7574517ff1f9d
wizardy <wizardhassan99@gmail.com>
2024-04-24 10:45:48 +0100

    commit

--- components/shared/SideBar.tsx
+++ components/shared/SideBar.tsx
@@ -1,9 +1,15 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 function SideBar() {
   return (
     <aside className='side-bar'>
-      SideBar
+      <div className="flex size-full flex-col gap-4">
+        <Link href='/' className="side-bar-logo">
+          <Image src="/assets/images/logo.png" alt='Smart Canvas Logo' width={180} height={28} />
+        </Link>
+      </div>
     </aside>
   )
 }
