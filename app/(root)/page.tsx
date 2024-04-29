df110ad9c704ccdcdc27ce3e1bbc26d462ca54f3
wizardy <wizardhassan99@gmail.com>
2024-04-29 11:07:24 +0100

    commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,9 @@
-import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 function Home() {
   return (
     <div>
       <p>Home</p>
-      <UserButton afterSignOutUrl='/'/>
     </div>
   )
 }
