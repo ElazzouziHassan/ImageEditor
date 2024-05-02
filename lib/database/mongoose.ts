9f58e213ef12211da3ddcb988fc60b61da7037d7
wizardy <wizardhassan99@gmail.com>
2024-05-02 10:58:33 +0100

    mongoose config & setup

---
+++ lib/database/mongoose.ts
@@ -0,0 +1,29 @@
+import mongoose, {Mongoose, connection} from "mongoose";
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+interface MongooseConnection {
+  connection: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+let cached: MongooseConnection = (global as any).mongoose;
+
+if( !cached ) {
+  cached = (global as any).mongoose = { connection: null, promise: null }
+}
+
+const connectToDB = async () => {
+  if (cached.connection) return cached.connection;
+
+  if (!MONGODB_URL) throw new Error ("MongoDB URL error : URL Not Defined !");
+
+  cached.promise = cached.promise || mongoose.connect(
+    MONGODB_URL,
+    // { DB_NAME:smartcanvas, bufferCommands:false }
+  );
+
+  cached.connection = await cached.promise;
+
+  return cached.connection;
+}
