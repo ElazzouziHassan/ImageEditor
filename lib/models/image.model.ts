950bc000a33d1f3969de045f45cb92c5e7e4e721
wizardy <wizardhassan99@gmail.com>
2024-05-02 11:22:58 +0100

    Image Model

---
+++ lib/models/image.model.ts
@@ -0,0 +1,43 @@
+import { Document, Schema, model, models } from "mongoose";
+
+export interface IImage extends Document {
+  title: string;
+  editingType: string;
+  publicId: string;
+  secureURL: string; 
+  width?: number;
+  height?: number;
+  config?: object; 
+  editingUrl?: string; 
+  aspectRatio?: string;
+  color?: string;
+  prompt?: string;
+  author: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+  }
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const ImageSchema = new Schema({
+  title: { type: String, required: true },
+  editingType: { type: String, required: true },
+  publicId: { type: String, required: true },
+  secureURL: { type: String, required: true },
+  width: { type: Number },
+  height: { type: Number },
+  config: { type: Object },
+  editingUrl: { type: String },
+  aspectRatio: { type: String },
+  color: { type: String },
+  prompt: { type: String },
+  author: { type: Schema.Types.ObjectId, ref: 'User' },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+});
+
+const Image = models?.Image || model('Image', ImageSchema);
+
+export default Image;
