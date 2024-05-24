a2d70260443d4418f0a0e039389cc3fe548e3be9
wizardy <wizardhassan99@gmail.com>
2024-05-24 10:38:56 +0100

    commit

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,8 +1,32 @@
+import { getUserImages } from '@/lib/actions/image.actions';
+import { getUserById } from '@/lib/actions/user.actions';
 import React from 'react'
+import Image from "next/image";
+import { redirect } from "next/navigation";
+import Header from '@/components/shared/Header';
+import { Collection } from '@/components/shared/Collection';
+import { auth } from '@clerk/nextjs/server';
 
-function Profile() {
+const Profile = async ({ searchParams }: SearchParamProps) => {
+
+  const page = Number(searchParams?.page) || 1;
+  const { userId } = auth();
+
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
+  const images = await getUserImages({ page, userId: user._id }); 
+  
   return (
-    <div>Profile</div>
+    <>
+     <section className="mt-8 md:mt-14">
+        <Collection
+          images={images?.data}
+          totalPages={images?.totalPages}
+          page={page}
+        />
+      </section>
+    </>
   )
 }
 
