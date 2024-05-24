954e920fc5549ce87bc240529b1736d78df43b58
wizardy <wizardhassan99@gmail.com>
2024-05-24 12:01:52 +0100

    commit

--- constants/index.ts
+++ constants/index.ts
@@ -123,28 +123,28 @@ export const editTypes = {
   restore: {
     type: "restore",
     title: "Restore Image",
-    subTitle: "Refine images by removing noise and imperfections",
+    subTitle: "Repair, Restore and Enhance Old images",
     config: { restore: true },
     icon: "image.svg",
   },
   removeBackground: {
     type: "removeBackground",
-    title: "Background Remove",
-    subTitle: "Removes the background of the image using AI",
+    title: "Background Remover",
+    subTitle: "Remove the background of the an image using AI",
     config: { removeBackground: true },
     icon: "camera.svg",
   },
   fill: {
     type: "fill",
     title: "Generative Fill",
-    subTitle: "Enhance an image's dimensions using AI outpainting",
+    subTitle: "Extend an image's dimensions using AI outpainting",
     config: { fillBackground: true },
     icon: "stars.svg",
   },
   remove: {
     type: "remove",
-    title: "Object Remove",
-    subTitle: "Identify and eliminate objects from images",
+    title: "Object Remover",
+    subTitle: "Identify and object and remove it from images",
     config: {
       remove: { prompt: "", removeShadow: true, multiple: true },
     },
@@ -153,7 +153,7 @@ export const editTypes = {
   recolor: {
     type: "recolor",
     title: "Object Recolor",
-    subTitle: "Identify and recolor objects from the image",
+    subTitle: "Identify and recolor objects from the an image",
     config: {
       recolor: { prompt: "", to: "", multiple: true },
     },
