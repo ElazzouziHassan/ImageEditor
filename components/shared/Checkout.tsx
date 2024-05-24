3707ab2891593b23a32454120960f9af6912c6c3
wizardy <wizardhassan99@gmail.com>
2024-05-24 10:39:02 +0100

    commit

---
+++ components/shared/Checkout.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { Button } from "../ui/button";
+
+const Checkout = () => {
+  return (
+    <form method="POST">
+      <section>
+        <Button>
+          Buy Credit
+        </Button>
+      </section>
+    </form>
+  );
+};
+
+export default Checkout;
