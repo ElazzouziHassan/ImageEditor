75c37308028d2128d924babfb983fd280ba313b4
wizardy <wizardhassan99@gmail.com>
2024-05-24 12:01:40 +0100

    commit

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -19,7 +19,38 @@ const Profile = async ({ searchParams }: SearchParamProps) => {
   
   return (
     <>
-     <section className="mt-8 md:mt-14">
+      <Header title="Profile"/>
+    
+      <section className="profile">
+        <div className="profile-balance">
+          <p className="p-14-medium md:p-16-medium">COINS BALANCE</p>
+          <div className="mt-4 flex items-center gap-4">
+            <Image
+              src="/assets/icons/coins.png"
+              alt="coins"
+              width={50}
+              height={50}
+              className="size-9 md:size-12"
+            />
+            <h2 className="h2-bold text-dark-600">{user.creditBalance}</h2>
+          </div>
+        </div>
+
+        <div className="profile-image-manipulation">
+          <p className="p-14-medium md:p-16-medium">Gallery</p>
+          <div className="mt-4 flex items-center gap-4">
+            <Image
+              src="/assets/icons/gallery.png"
+              alt="Gallery"
+              width={50}
+              height={50}
+              className="size-9 md:size-12"
+            />
+            <h2 className="h2-bold text-dark-600">{images?.data.length}</h2>
+          </div>
+        </div>
+      </section>
+      <section className="mt-8 md:mt-14">
         <Collection
           images={images?.data}
           totalPages={images?.totalPages}
