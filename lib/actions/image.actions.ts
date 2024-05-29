e00a61d5322034098b24241173660c823c84df3d
wizardy <wizardhassan99@gmail.com>
2024-05-29 19:53:11 +0100

    cloudinary res setup&config

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -7,7 +7,7 @@ import User from "../database/models/user.model";
 import Image from "../database/models/image.model";
 import { redirect } from "next/navigation";
 
-// import { v2 as cloudinary } from 'cloudinary';
+import { v2 as cloudinary } from 'cloudinary';
 
 const populateUser = (query: any) => query.populate({
   path: 'author',
@@ -100,12 +100,12 @@ export async function getAllImages({ limit = 9, page = 1, searchQuery = '' }: {
   try {
     await connectToDatabase();
 
-    // cloudinary.config({
-    //   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-    //   api_key: process.env.CLOUDINARY_API_KEY,
-    //   api_secret: process.env.CLOUDINARY_API_SECRET,
-    //   secure: true,
-    // })
+    cloudinary.config({
+      cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+      secure: true,
+    })
 
     let expression = 'folder=scanvas';
 
@@ -113,18 +113,18 @@ export async function getAllImages({ limit = 9, page = 1, searchQuery = '' }: {
       expression += ` AND ${searchQuery}`
     }
 
-    // const { resources } = await cloudinary.search
-    //   .expression(expression)
-    //   .execute();
+    const { resources } = await cloudinary.search
+      .expression(expression)
+      .execute();
 
-    // const resourceIds = resources.map((resource: any) => resource.public_id);
+    const resourceIds = resources.map((resource: any) => resource.public_id);
 
     let query = {};
 
     if(searchQuery) {
       query = {
         publicId: {
-          // $in: resourceIds
+          $in: resourceIds
         }
       }
     }
